b4ccf7c5196db01877464d2db517e7afe4ca7bb5
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 09:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   var len = katzDeliLine.length;
-  var i = 0l
+  var i = 0;
   console.log(`length is: ${len}`)
   if(len===0){
     return "The line is currently empty."
